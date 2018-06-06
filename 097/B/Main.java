1e65c391c73f35d0bba5f46031c391a3c963b741
kmichy <aaaa0424pg@gmail.com>
2018-06-06 22:14:50 +0900

    fix

--- 097/B/Main.java
+++ 097/B/Main.java
@@ -1,22 +1,21 @@
-import java.util.*;
+import java.util.Scanner;;
 
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = Integer.parseInt(sc.nextLine());
-        String b = sc.nextLine();
-        int max = 0;
-        for(int i = 0;i < a;i++){
-            String head = b.substring(0,i);
-            String tail = b.substring(i);
-            int count = 0;
-            for(int j = 'a';j<'z';j++){
-                if(head.indexOf(j)!= -1 && tail.indexOf(j) != -1){
-                    count++;
+        int a = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        for(int i = 1;i<=1000;i++){
+            for(int j = 2;j<20;j++){
+                int tmp = (int)Math.pow(i,j);
+                if(tmp>a){
+                    break;
+                }
+                if(max < tmp && tmp<=a){
+                    max = tmp;
                 }
             }
-            max = Math.max(count,max);
         }
-    System.out.println(max);
+        System.out.println(max);
     }
 }
