3cc08e9daf3f72721a74f8696626fdc3a777d4c7
kmichy <aaaa0424pg@gmail.com>
2018-06-06 21:20:20 +0900

    maru

--- 096/B/Main.java
+++ 096/B/Main.java
@@ -1,1 +1,20 @@
-test
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int max = a;
+        if(max < b){
+            max = b;
+        }
+        if(max < c){
+            max = c;
+        }
+        int answer = a+b+c-max +max*(int)(Math.pow(2,d));
+        System.out.println(answer);
+    }
+}
